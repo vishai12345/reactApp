b1665210d8332754abb2497ff5ba05f521eb8630
Vishal chaturvedi <mahi.patidar13@gmail.com>
2019-05-09 14:54:15 +0530

    update component

--- src/component/Home.js
+++ src/component/Home.js
@@ -8,9 +8,6 @@ class Home extends Component {
 		          <h1>Navbar examples</h1>
 		          <p>This example is a quick exercise to illustrate how the navbar and its contents work. Some navbars extend the width of the viewport, others are confined within a <code>.container</code>. For positioning of navbars, checkout the <a href="../navbar-top/">top</a> and <a href="../navbar-top-fixed/">fixed top</a> examples.</p>
 		          <p>At the smallest breakpoint, the collapse plugin is used to hide the links and show a menu button to toggle the collapsed content.</p>
-		          <p>
-		            <a className="btn btn-primary" href="#" role="button">View navbar docs »</a>
-		          </p>
 		        </div>
 		      </div>
       )
